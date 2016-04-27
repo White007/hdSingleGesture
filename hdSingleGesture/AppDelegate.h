b7d2d1a8a3db7b42d7ea94a4de8174b07837712b
zhumingke <ifeiteng007@sina.com>
2016-04-27 09:48:06 +0800

    Initial Commit

---
+++ hdSingleGesture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hdSingleGesture
+//
+//  Created by zhumingke on 16/4/27.
+//  Copyright © 2016年 zhumingke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hdSingleGesture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hdSingleGesture
+//
+//  Created by zhumingke on 16/4/27.
+//  Copyright © 2016年 zhumingke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
